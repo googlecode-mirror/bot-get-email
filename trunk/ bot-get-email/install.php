3a18815fff0d6da1dacaffc4559595d21fa1d671
duyet2000@gmail.com <duyet2000@gmail.com@b2dc0c02-23b4-195f-ad2a-a2c9ca65a931>
2012-02-20 07:58:54 +0000

    Check install

--- trunk/ bot-get-email/install.php
+++ trunk/ bot-get-email/install.php
@@ -7,3 +7,32 @@
 
 require_once('database.class.php');
 require_once('functions.php');
+if( file_exits('config.php') )
+{
+	@unlink('config.php');
+}
+if( !$_POST['submit'] )
+{
+	yplitgroup_view_form_install(array('dbhost'=>'','dbname'=>'','dbuname'=>'','dbpass'=>''), '');
+}
+else
+{
+	$_POST['dbhost'] = !empty($_POST['dbhost'])?$_POST['dbhost']:'';
+	$_POST['dbname'] = !empty($_POST['dbname'])?$_POST['dbname']:'';
+	$_POST['dbuname'] = !empty($_POST['dbuname'])?$_POST['dbuname']:'';
+	$_POST['dbpass'] = !empty($_POST['dbpass'])?$_POST['dbpass']:'';
+	
+	if( $_POST['dbhost'] == '' )
+	{
+		yplitgroup_view_form_install($_POST, 'dbhost Empty!!!');
+	}
+	elseif( $_POST['dbname'] == '' )
+	{
+		yplitgroup_view_form_install($_POST, 'dbname Empty!!!');
+	}
+	elseif( $_POST['dbuname'] == '' )
+	{
+		yplitgroup_view_form_install($_POST, 'dbuname Empty!!!');
+	}
+
+}
