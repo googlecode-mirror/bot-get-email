059cdfb6a43b8989697644e5c901ec916814cab6
duyet2000@gmail.com <duyet2000@gmail.com@b2dc0c02-23b4-195f-ad2a-a2c9ca65a931>
2012-02-20 07:22:59 +0000

    Them class $C config.

--- trunk/ bot-get-email/index.php
+++ trunk/ bot-get-email/index.php
@@ -4,6 +4,25 @@
 * Yplitgroup
 * 20/2/2012
 */
+/**
+* Auto Get Email Tool
+* This Tools run in PHP. This will get all email
+* from a site input. They also get all URL from 
+* this site, and save it to database to get 
+* later. That will increase email result too.
+* Email will print in result page. You can set 
+* crontjob for this tool.
+* 
+* If you want donate for me. Please email 
+* duyet2000@gmail.com
+* Website: http://lemon9x.com
+*/
+// ### C Config ###
+$C = new stdClass;
+$C->database = array();
+$C->action = '';
+
+// ### INCLUDE ###
 require_once('database.class.php');
 require_once('simple_html_dom.php');
 require_once('config.php');
