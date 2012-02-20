f46e12fca0bc2163cbad0f92290eae84633eb666
duyet2000@gmail.com <duyet2000@gmail.com@b2dc0c02-23b4-195f-ad2a-a2c9ca65a931>
2012-02-20 07:58:12 +0000

    Them ham yplitgroup_view_form_install()

--- trunk/ bot-get-email/functions.php
+++ trunk/ bot-get-email/functions.php
@@ -19,3 +19,76 @@ function yplitgroup_redirect_install()
 	@header('Location: ./install.php');
 	die();
 }
+
+// ### INSTALL ###
+// View form for install
+function yplitgroup_view_form_install($POST, $error)
+{
+	$mt[0] = htmlspecialchars('->');
+	$mt[1] = htmlspecialchars('[\'');
+	$mt[2] = htmlspecialchars('\']');
+	$error = isset($error)?$error:'';
+	echo <<<Y
+<html>
+	<head>
+		<title>AUTO GET EMAIL - INSTALL - Power by Yplitgroup</title>
+	</head>
+	<style>
+		.error{font-weight:bold; color: red;}
+	</style>
+	<body>
+		<h1>Install</h1>
+		<form action="" method="POST">
+			<table border=0>
+				<tr>
+					<td>
+						<span class="error">{$error}</span>
+					</td>
+				</tr>
+				<tr>
+					<td width="40%">
+						<b>\$C{$mt[0]}database{$mt[1]}dbhost{$mt[2]}</b>
+					</td>
+					<td>
+						<input type='text' maxlength=100 name='dbhost' value="{$POST[dbhost]}">
+					</td>
+				<tr>
+				<tr>
+					<td width="40%">
+						<b>\$C{$mt[0]}database{$mt[1]}dbname{$mt[2]}</b>
+					</td>
+					<td>
+						<input type='text' maxlength=100 name='dbname' value="{$POST[dbname]}">
+					</td>
+				<tr>
+				<tr>
+					<td width="40%">
+						<b>\$C{$mt[0]}database{$mt[1]}dbuname{$mt[2]}</b>
+					</td>
+					<td>
+						<input type='text' maxlength=100 name='dbuname' value="{$POST[dbuname]}">
+					</td>
+				<tr>
+				<tr>
+					<td width="40%">
+						<b>\$C{$mt[0]}database{$mt[1]}dbpass{$mt[2]}</b>
+					</td>
+					<td>
+						<input type='text' maxlength=100 name='dbpass' value="{$POST[dbpass]}">
+					</td>
+				<tr>
+				<tr>
+					<td>
+						
+					</td>
+					<td>
+						<input type='submit' name='submit'>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
+Y;
+
+}
