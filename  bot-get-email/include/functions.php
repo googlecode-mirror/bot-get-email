86cecd5c8e6a458b51c58618fc1a209e31335d61
duyet2000 <duyet2000@gmail.com>
2012-02-21 14:51:31 +0000

    Them ham yplitgroup_crontjob(), yplitgroup_get_email(),yplitgroup_bot_get_url(), yplitgroup_disable_url()

---  bot-get-email/include/functions.php
+++  bot-get-email/include/functions.php
@@ -5,6 +5,144 @@
 * 20/2/2012
 */
 
+// Cront job
+function yplitgroup_crontjob()
+{
+	global $C;
+	if( !yplitgroup_check_time( ) ) return false;
+	
+	// Start work
+	$q = "SELECT `id`,`url` FROM `yplitgroup_global_url` WHERE `active` = 1 LIMIT " . $C->constant->link_get_limit;
+	$C->db->sql_query( $q );
+	$result = array();
+	while( $re = $C->db->sql_fetch_assoc() )
+	{
+		$t1 = time();
+		$result[]['Id'] = $re['id'];
+		$result[]['Url'] = $re['url'];
+		yplitgroup_get_email( $re['url'] ); 
+		yplitgroup_bot_get_url( $re['url'] ); 
+		yplitgroup_disable_url( $re['id'] );
+		$result[]['Time'] = time()-$t1;
+	}
+	
+	yplitgroup_update_time( );
+	echo "<hr>";
+	foreach($result as $re)
+	{
+		echo "[ $re[id] ] Url: $re[Url] ($re[Time])";
+	}
+}
+
+// BOT: Disable url
+function yplitgroup_disable_url( $id )
+{
+	global $C;
+	$id = (int) $id;
+	$q = "UPDATE `yplitgroup_global_url` SET `active`=0 WHERE `id` = $id ";
+	return $C->db->sql_query( $q );
+}
+
+// BOT: Auto get url
+function yplitgroup_bot_get_url( $url )
+{
+	global $C; 
+	if( empty( $url ) ) return false;
+	// Get all url
+	$html = file_get_html( $url ); 
+	$this_host = parse_url( $url );
+	$this_host = $this_host['scheme'] . '://' . $this_host['host'] . '/';
+	foreach( $html->find('a') as $a )
+	{
+		if( !empty( $a->href ) and !preg_match('/^#/', $a->href) )
+		{
+			if( !preg_match( '/^http:\/\//is', $a->href ) )
+			{
+				$a->href = $this_host . $a->href;
+			}
+			$q = "SELECT 1 FROM `yplitgroup_global_url` WHERE `url` = " . $C->db->dbescape_string( $a->href );
+			$C->db->sql_query( $q );
+			if( $C->db->sql_numrows() == 0 ) // Chua ton tai url nay
+			{ 
+				$q = "INSERT INTO `yplitgroup_global_url`(`url`, `active`) VALUE( " . $C->db->dbescape_string( $a->href ) . ", 1 ) ";
+				$C->db->sql_query( $q );
+			}
+		}
+	}
+}
+
+// BOT: Auto get email
+function yplitgroup_get_email( $url )
+{
+	global $C;
+	if( empty( $url ) ) return false;
+	$html = file_get_html( $url );
+	$text = $html->plaintext;
+	$email_preg = '/(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))/'; // Power by Nukeviet :)
+	preg_match_all( $email_preg, $text, $email );
+	$email = $email[0];
+	foreach( $email as $_email )
+	{ //die($_email);
+		$q = "SELECT 1 FROM `yplitgroup_email` WHERE `email` = " . $C->db->dbescape_string( $_email );
+		$C->db->sql_query( $q );
+		if( $C->db->sql_numrows() == 0 ) // Chua ton tai url nay
+		{
+			$q = "INSERT INTO `yplitgroup_email`(`email`) VALUE( " . $C->db->dbescape_string( $_email ) . ") ";
+			$C->db->sql_query( $q );
+		}
+	}
+}
+
+// Check time (cront job)
+function yplitgroup_check_time()
+{
+	global $C;
+	$f = $C->constant->last_run_file;
+	$fp = fopen($f, 'r');
+	$lastrun = (int)@fread( $fp, filesize( $f ) );
+	if( ( time() - $lastrun ) < $C->constant->time_autorun ) return false;
+	return true;
+}
+
+
+// Update auto run time
+function yplitgroup_update_time()
+{
+	global $C;
+	$f = $C->constant->last_run_file;
+	$fp = fopen($f, "w");
+	fwrite( $fp, time() );
+	fclose( $fp );
+	return true;
+}
+
+// View main page
+function yplitgroup_view_main()
+{
+	echo "<h3>Auto BOT</h3><br />";
+	echo "
+<ul>
+	<li>
+		<a href='index.php?action=help'>index.php?action=help</a>: View help page
+	</li>
+	<li>
+		<a href='index.php?action=list'>index.php?action=list</a>: View list email
+		<ul>
+			<li>
+				<a href='index.php?action=list'>index.php?action=list&viewtype=file</a>: Download file txt list email 
+			</li>
+			<li>
+				<a href='index.php?action=list'>index.php?action=list&viewtype=textarea</a>: View textarea
+			</li>
+			<li>
+				<a href='index.php?action=list'>index.php?action=list&viewtype=emailsent</a>: View textarea like email Senter (to copy)
+			</li>
+		</ul>
+	</li>
+</ul>
+";
+}
+
 // View list email as sent page
 function yplitgroup_view_emailsent()
 {
@@ -50,7 +188,7 @@ function yplitgroup_view_file( $file, $autoDownload = true, $limit=0 )
 		$q = "SELECT `email` FROM `yplitgroup_email`";
 	}
 	$C->db->sql_query( $q );
-	$f = empty( $file ) ?  ( time() . '.txt' ) : $file;
+	$f = empty( $file ) ?  ( $C->constant->list_file_default ) : $file;
 	$fp = @fopen( $f, 'w' );
 	while( $re = $C->db->sql_fetch_assoc() )
 	{
