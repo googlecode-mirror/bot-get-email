6ce054e9869000e66cb6501501869c2ec945cc13
duyet2000@gmail.com <duyet2000@gmail.com@b2dc0c02-23b4-195f-ad2a-a2c9ca65a931>
2012-02-21 13:03:11 +0000

    Them cac cau lenh thong qua uri query

--- trunk/ bot-get-email/index.php
+++ trunk/ bot-get-email/index.php
@@ -23,9 +23,9 @@ $C->database = array();
 $C->action = '';
 
 // ### INCLUDE ###
-require_once('database.class.php');
-require_once('functions.php');
-require_once('simple_html_dom.php');
+require_once('include/database.class.php');
+require_once('include/functions.php');
+require_once('include/simple_html_dom.php');
 require_once('config.php');
 require_once('bot.php');
 
@@ -37,4 +37,45 @@ if( !yplitgroup_check_install() )
 {
 	yplitgroup_redirect_install();
 }
+
+// ### CHECK SQL ###
+if( count( $C->db->error ) )
+{
+	die( "SQL ERROR!!!<hr>" . $C->db->error['user_message']);
+}
+
+// ### GET QUERY THOUGHT GET METHOD ###
+$C->action = ( !empty( $_GET['action'] ) ) ? $_GET['action'] : 'none';
+
+// ### DO REQUEST ###
+	// ### HELP CONSOLE ###
+		if( $C->action == 'help' OR $C->action == 'h' )
+		{
+			yplitgroup_view_help();
+			die();
+		}
+	
+	// ### VIEW LIST EMAIL ###
+		if( $C->action == 'list' OR $C->action == 'l' )
+		{
+			if( isset( $_GET['viewtype'] ) and !empty( $_GET['viewtype'] ) and $_GET['viewtype'] == 'file' )
+			{
+				yplitgroup_view_file('email_List.txt', true, 0); 
+				die();
+			}
+			elseif( isset( $_GET['viewtype'] ) and !empty( $_GET['viewtype'] ) and $_GET['viewtype'] == 'textarea' )
+			{
+				yplitgroup_view_textarea(); 
+				die();
+			}
+			elseif( isset( $_GET['viewtype'] ) and !empty( $_GET['viewtype'] ) and $_GET['viewtype'] == 'emailsent' )
+			{
+				yplitgroup_view_emailsent(); 
+				die();
+			}
+			else
+			{
+			echo '4';
+			}
+		}
 
