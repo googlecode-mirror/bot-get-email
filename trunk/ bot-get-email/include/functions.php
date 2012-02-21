5822f8570c903849bd6ccf291968e84af589b22d
duyet2000@gmail.com <duyet2000@gmail.com@b2dc0c02-23b4-195f-ad2a-a2c9ca65a931>
2012-02-21 13:02:32 +0000

    Add function to functions.php

--- trunk/ bot-get-email/include/functions.php
+++ trunk/ bot-get-email/include/functions.php
@@ -5,6 +5,99 @@
 * 20/2/2012
 */
 
+// View list email as sent page
+function yplitgroup_view_emailsent()
+{
+	global $C;
+	$q = "SELECT `email` FROM `yplitgroup_email`";
+	$C->db->sql_query( $q );
+	echo "<h3>Auto BOT Result</h3><br />";
+	echo "<textarea style='width:100%; height:98%; color:#15c; padding:10px;'>";
+	while( $re = $C->db->sql_fetch_assoc() )
+	{
+		echo $re['email'] . ", ";
+	}
+	echo "</textarea>";
+	return;
+}
+
+// View list email as textarea
+function yplitgroup_view_textarea()
+{
+	global $C;
+	$q = "SELECT `email` FROM `yplitgroup_email`";
+	$C->db->sql_query( $q );
+	echo "<h3>Auto BOT Result</h3><br />";
+	echo "<textarea style='width:100%; height:98%; color:#15c; padding:10px;'>";
+	while( $re = $C->db->sql_fetch_assoc() )
+	{
+		echo $re['email'] . "\n";
+	}
+	echo "</textarea>";
+	return;
+}
+
+// View list email as file
+function yplitgroup_view_file( $file, $autoDownload = true, $limit=0 )
+{
+	global $C;
+	if( $limit !=0 )
+	{
+		$q = "SELECT `email` FROM `yplitgroup_email` LIMIT 0, $limit";
+	}
+	else
+	{
+		$q = "SELECT `email` FROM `yplitgroup_email`";
+	}
+	$C->db->sql_query( $q );
+	$f = empty( $file ) ?  ( time() . '.txt' ) : $file;
+	$fp = @fopen( $f, 'w' );
+	while( $re = $C->db->sql_fetch_assoc() )
+	{
+		@fwrite( $fp, $re['email'] . "\n" );
+	}
+	@fclose( $fp );
+	if( $autoDownload )
+	{
+		$fp = @fopen( $f, 'r' );
+		header('Content-disposition: attachment; filename="'.$f.'"');
+		@header("Content-type: application/octet-stream");
+		@header("Content-length: " . @filesize($f)); 
+		@fpassthru($fp);
+		@fclose($fp);
+	}
+	else
+	{
+		return $f;
+	}
+}
+
+// View help page
+function yplitgroup_view_help()
+{
+	echo "
+<html>
+<head>
+	<title> ### HELP BOT ### </title>
+</head>
+<body>
+	<h1>
+		AUTO BOT SYSTEM
+	</h1>
+	<br />
+	<ul>
+		<li>
+			<a href='index.php?action=help'>index.php?action=help</a>: View help page
+		</li>
+		<li>
+			<a href='index.php?action=list'>index.php?action=list</a>: View list 
+		</li>
+	</ul>
+</body>
+</html>
+";
+}
+
 // Check install
 function yplitgroup_check_install()
 {
